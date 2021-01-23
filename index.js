268d71d3a8cd41c8e9e4aaa9e0e7c16bb6bbb9bd
aashayshah160499 <aashay.shah@somaiya.edu>
2021-01-24 04:24:52 +0530

    Sound Added

---
+++ index.js
@@ -0,0 +1,41 @@
+var no_of_buttons = document.querySelectorAll(".drum").length;
+for(var i=0; i<no_of_buttons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var audio;
+    switch (this.innerHTML) {
+      case "w":
+        audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+      break;
+      case "a":
+        audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+      break;
+      case "s":
+        audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+      break;
+      case "d":
+        audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+      break;
+      case "j":
+        audio = new Audio("sounds/snare.mp3");
+        audio.play();
+      break;
+      case "k":
+        audio = new Audio("sounds/crash.mp3");
+        audio.play();
+      break;
+      case "l":
+        audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+      break;
+      default:
+        console.log(this.innerHTML);
+    }
+  });
+}
+
+var audio = new Audio("sounds/tom-1.mp3");
+audio.play();
